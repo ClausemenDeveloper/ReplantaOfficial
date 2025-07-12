270ef264998960f85701940638c343664f1c37cf
ClausemenDeveloper <151139919+ClausemenDeveloper@users.noreply.github.com>
2025-07-12 06:18:04 +0100

    dia 6 atualizacao

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { handleDemo } from "./routes/demo";
+import { handleDemo } from "./routes/demo.js";
 
 // Dynamic imports for MongoDB routes to avoid startup issues
 let databaseService: any = null;
